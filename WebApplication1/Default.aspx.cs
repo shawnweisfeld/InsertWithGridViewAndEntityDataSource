7f90a6b152ccf19238a64b4df44f27757dda8ca7
Shawn Weisfeld <shawn@shawnweisfeld.com>
2013-01-31 19:52:41 -0600

    Initial Commit

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.UI;
+
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void FormView1_ItemInserted(object sender, System.Web.UI.WebControls.FormViewInsertedEventArgs e)
+        {
+            grdMain.DataBind();
+        }
+
+
+
+
+    }
+
+   
+}
